f07d0030a7e38d54f2e55ce6596599c84b4c5bb1
Bozhao Li <youhusky@126.com>
2016-05-27 14:23:37 -0700

    Leetcode

---
+++ 145-Binary-Tree-Postorder-Traversal/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        helper(rot, res);
+        return res;
+    }
+    private void helper(TreeNode root, ArrayList<Integer> res){
+        if (root == null)
+            return;
+        helper(root.left, res);
+        helper(root.right, res);
+        rea.add(root.val);
+        
+    }
+}
