e7a76c331b73c1ba270e7687832ab79c456aea34
Bozhao Li <youhusky@126.com>
2016-05-27 16:01:07 -0700

    Leetcode

---
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private int depth;
+    public int maxDepth(TreeNode root) {
+        if (root ==null)
+            return 0;
+        depth = 0;
+        dfs(root,1);
+        return depth;
+    }
+    private void dfs(TreeNode root, int curr){
+        if (root == null){
+            return;
+        }
+        if (depth < curr)
+            depth = curr;
+        dfs(root.left, curr + 1);
+        dfs(root.right, curr + 1);
+    }
+}
