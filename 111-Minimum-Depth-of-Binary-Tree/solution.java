4299860e8c0a7459be433ede78034c67cbc8c99c
Bozhao Li <youhusky@126.com>
2016-05-27 16:08:23 -0700

    Leetcode

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -9,11 +9,11 @@
  */
 public class Solution {
     public int minDepth(TreeNode root) {
-        if(!root)
+        if(root == null)
             return 0;
-        if(!root.left)
+        if(root.left == null)
             return minDepth(root.right)+1;
-        if(!root.right)
+        if(root.right ==null)
             return minDepth(root.left) + 1;
         return Math.min(minDepth(root.left), minDepth(root.right))+1;
     }
