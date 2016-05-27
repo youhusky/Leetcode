008536366a61dbdb1b1d35eaa4a31d70c80a178f
Bozhao Li <youhusky@126.com>
2016-05-27 16:25:59 -0700

    Leetcode

--- 110-Balanced-Binary-Tree/solution.java
+++ 110-Balanced-Binary-Tree/solution.java
@@ -19,8 +19,7 @@ public class Solution {
 
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        System.out.println(left);
-        if (Math.abs(left-right) > 1) {
+        if (left == -1 || right == -1 ||Math.abs(left-right) > 1) {
             return -1;
         }
         return Math.max(left, right) + 1;
