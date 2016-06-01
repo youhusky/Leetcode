df0e72c06cb8a7232149d0c39e7d9b27c77ba8d7
Bozhao Li <bozhaoli@usc.edu>
2016-06-01 00:03:51 -0700

    Leetcode

---
+++ 15-3Sum/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i=0; i< nums.length; i++){
+            if (i != 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0){
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    res.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                }
+                else if(sum<0) left++;
+                else if(sum>0) right--;
+            }
+        }
+        return res;
+    }
+}
