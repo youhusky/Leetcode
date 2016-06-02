7f196a18d0b155baa6b0b302685de888e401cfd7
Bozhao Li <bozhaoli@usc.edu>
2016-06-02 16:00:14 -0700

    Leetcode

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int reverse(int x) {
         long temp = 0;
-        
+        // use long to check overflow
         while (x != 0){
-            
-            temp = temp * 10;
+            temp = temp * 10; //123->(3*10 + 2) *10 + 1
             temp += x % 10;
             x/= 10;
         }
         if (temp > Integer.MAX_VALUE || temp < Integer.MIN_VALUE)
+        // check overflow
                 return 0;
         return (int) temp;
     }
