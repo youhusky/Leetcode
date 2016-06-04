441cdcbe20a6400dc8237deb3c8ed58e69ef8848
Bozhao Li <bozhaoli@usc.edu>
2016-06-04 01:36:13 -0700

    Leetcode

--- 113-Path-Sum-II/solution.java
+++ 113-Path-Sum-II/solution.java
@@ -8,19 +8,22 @@
  * }
  */
 public class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        List<Integer> path = new ArrayList();
-        findPath(root, sum, path, ans);
-        return ans;
-    }
+    private List<List<Integer>> result = new ArrayList<List<Integer>>();
 
-    private void findPath(TreeNode r, int target, List<Integer> path, List<List<Integer>> ans) {
-        if (r == null ) return;
-        path.add(r.val);
-        if (r.left == null && r.right == null && r.val == target) ans.add(new ArrayList<Integer>(path));
-        if (r.right != null) findPath(r.right, target - r.val, path, ans);
-        if (r.left != null) findPath(r.left, target - r.val, path, ans);
-        path.remove(path.size() - 1);
+public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    helper(new ArrayList<Integer>(), root, sum);
+    return result;
+}
+
+private void helper(List<Integer> list, TreeNode root, int sum) {
+    if (root == null) return;
+    list.add(root.val);
+    sum -= root.val;
+    if (root.left == null && root.right == null) {
+        if (sum == 0) result.add(list);
+        return;
     }
+    helper(new ArrayList<Integer>(list), root.left, sum);
+    helper(new ArrayList<Integer>(list), root.right, sum);
+}
 }
