fe74a56d8934b3976fe1469ec9184e678ce39180
Bozhao Li <bozhaoli@usc.edu>
2016-06-06 17:46:30 -0700

    Leetcode

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+    }
+}
