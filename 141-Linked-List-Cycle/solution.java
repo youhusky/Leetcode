5a528d5e67a944d4d2b3f5c7c5ca117ad11a0496
Bozhao Li <bozhaoli@usc.edu>
2016-06-09 18:38:07 -0700

    Leetcode

---
+++ 141-Linked-List-Cycle/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        if (head == null)    return false;
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
