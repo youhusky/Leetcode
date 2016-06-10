65ea8a0340cb510fecefa80cc461dec4141abb15
Bozhao Li <bozhaoli@usc.edu>
2016-06-10 16:20:47 -0700

    Leetcode

--- 92-Reverse-Linked-List-II/solution.java
+++ 92-Reverse-Linked-List-II/solution.java
@@ -6,6 +6,15 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+    //  //保存p3
+    // n = p2->next;
+    // //将p3的next挂载到p2后面
+    // p2->next = p3->next;
+    // //将p3挂载到p1的后面
+    // p1->next = p3;
+    // //将p2挂载到p3得后面
+    // p3->next = p2;
+//  对于上题，我们首先遍历得到第m - 1个node，也就是pm的前驱节点。然后依次遍历，处理挂载问题就可以了。
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null)   return null;
@@ -20,7 +29,7 @@ public class Solution {
             ListNode tmv = temp.next;
             temp.next = tmv.next;
             tmv.next = node.next;
-            node.next = tmv;
+            node.next = tmv;//1->
         }
         return dummy.next;
     }
