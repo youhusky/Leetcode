caa59c13ef5443d6b512988a03d2077a7c3c88de
Bozhao Li <bozhaoli@usc.edu>
2016-06-15 16:57:50 -0700

    Leetcode

---
+++ 204-Count-Primes/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int countPrimes(int n) {
+        boolean notPrime[] = new boolean[n+2];
+        notPrime[0] = notPrime[1] = true;
+        for (int i = 2; i*i < n; i++){
+            if (!notPrime[i]){
+            int c = i * i;
+            while (c < n){
+                notPrime[c] = true;
+                c += i;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++){
+            if (!notPrime[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
