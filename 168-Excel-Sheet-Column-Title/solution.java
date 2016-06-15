63749a1327779b363b580f48eb1080aebf22cf6b
Bozhao Li <bozhaoli@usc.edu>
2016-06-15 15:52:08 -0700

    Leetcode

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -1,6 +1,14 @@
 public class Solution {
     public String convertToTitle(int n) {
-        if (n == 0) return "";
-        return convertToTitle((n - 1) / 26) + (char) ((n - 1) % 26 + 'A');
+        // //recursive method      27 -- 1 (0 + 'A')+ 'A'
+        // if (n == 0) return "";
+        // return convertToTitle((n - 1) / 26) + (char) ((n - 1) % 26 + 'A');
+        StringBuilder res = new StringBuilder();
+        while (n > 0){
+            n--;
+            res.insert(0,(char) (n % 26 + 'A'));
+            n /= 26;
+        }
+        return res.toString();
     }
 }
