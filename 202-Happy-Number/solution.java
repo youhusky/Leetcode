e5bd4c53f8cf563e29648732a80e79f4220c8e18
Bozhao Li <bozhaoli@usc.edu>
2016-06-15 12:37:27 -0700

    Leetcode

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,22 +1,18 @@
 // use set to save the number if the number shows again but not 1 return false
 public class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> s = new HashSet<Integer>();
-        while (n != 1){
-            int temp = 0;
-            while (n>0){
-                temp += (n%10)*(n%10);
-                n /= 10;
-            }
-            n = temp;
-            if (s.contains(n)){
-                break;
-            }
-            else{
-                s.add(n);
-            }
+        if (n <= 0) return false;
+    HashSet<Integer> set = new HashSet<Integer>();
+    while(n !=1 && !set.contains(n)) {
+        set.add(n);
+        int m = n;
+        n = 0;
+        while(m != 0) {
+            n += (m%10)*(m%10);
+            m = m/10;
         }
-        System.out.println(s);
-        return (n == 1);
+    }
+    if (n == 1) return true;
+    else return false;
     }
 }
