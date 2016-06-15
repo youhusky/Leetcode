5a516078fed443fb5ac3b0c0016f8a69b319a27a
Bozhao Li <bozhaoli@usc.edu>
2016-06-15 15:04:32 -0700

    Leetcode

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -9,3 +9,18 @@ public class Solution {
        }
     }
 }
+// Iterative
+// public class Solution {
+//     public int trailingZeroes(int n) {
+//         if (n < 0) {
+//             return -1;
+//         }
+
+//         int count = 0;
+//         for (; n > 0; n /= 5) {
+//             count += (n / 5);
+//         }
+
+//         return count;
+//     }
+// }
