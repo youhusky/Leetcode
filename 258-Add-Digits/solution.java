1c2742a49cf7fec822d2ca9c89134d6d15f1fd1b
Bozhao Li <bozhaoli@usc.edu>
2016-06-15 16:15:24 -0700

    Leetcode

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,3 +1,14 @@
+// in  out  in  out
+// 0   0    10  1
+// 1   1    11  2
+// 2   2    12  3
+// 3   3    13  4
+// 4   4    14  5
+// 5   5    15  6
+// 6   6    16  7
+// 7   7    17  8
+// 8   8    18  9
+// 9   9    19  1
 public class Solution {
     public int addDigits(int num) {
         //out = (in - 1) % 9 + 1
