9e2b368940725e85fbf11725d2b755058ba0b579
Bozhao Li <bozhaoli@usc.edu>
2016-06-14 23:41:01 -0700

    Leetcode

--- 342-Power-of-Four/solution.java
+++ 342-Power-of-Four/solution.java
@@ -1,5 +1,17 @@
 public class Solution {
     public boolean isPowerOfFour(int num) {
-        return (num > 0 && (Math.log10(num) / Math.log10(4)) % 1 == 0);
+        // method 1 log
+        //return (num > 0 && (Math.log10(num) / Math.log10(4)) % 1 == 0);
+        
+        // method 2 bit
+        // 1 on the odd location 4 - 100
+        // 1   0 0000 0001
+        // 4   0 0000 0100
+        // 16  0 0001 0000
+        // 64  0 0100 0000
+        // 256 1 0000 0000
+        // 0x55555555   10101010101010101
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) == num;
+
     }
 }
