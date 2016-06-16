4abe3b00c6ec2afa23885862b54fa830cb9602a0
Bozhao Li <bozhaoli@usc.edu>
2016-06-16 16:53:45 -0700

    Leetcode

---
+++ 268-Missing-Number/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        //if (nums.length == 1)   return 0;
+        int length = nums.length;
+        int execption = length * (length+1) / 2;
+        int real = 0;
+        for (int i:nums){
+            real +=i;
+        }
+        System.out.println(real);
+       
+        return execption - real;
+    }
+}
