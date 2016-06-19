4683353036135e3acd8a095266d5cf25bfba6032
Bozhao Li <bozhaoli@usc.edu>
2016-06-18 23:57:11 -0700

    Leetcode

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,13 +1,15 @@
+// 101 101  101 110
+//用大小为 32的数组来记录所有 位上的和
 public class Solution {
     public int singleNumber(int[] nums) {
         int[] bit = new int[32];
         int res = 0;
         for (int i=0; i<32; i++){
             for(int j = 0; j < nums.length; j++){
-                if(((nums[j]>>i) & 1 )== 1)
+                if(((nums[j]>>i) & 1 )== 1) //为1 
                     bit[i]++;
             }
-            res |= (bit[i]%3) << i; 
+            res |= (bit[i]%3) << i; //倒序
         }
         return res;
     }
