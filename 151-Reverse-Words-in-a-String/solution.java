97a45247582d5b0ec587af5cc6820e8189aa2d4e
Bozhao Li <bozhaoli@usc.edu>
2016-06-20 15:40:58 -0700

    Leetcode

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public String reverseWords(String s) {
       // we can do better than one pass
+      //ab cd ef
       StringBuilder reversed = new StringBuilder();
       int j = s.length();
       for(int i = s.length()-1; i>=0;i--){
@@ -9,7 +10,9 @@ public class Solution {
           }
           else if (i == 0|| s.charAt(i-1)==' '){
               if(reversed.length() != 0)  reversed.append(' ');
-              reversed.append(s.substring(i,j));
+              reversed.append(s.substring(i,j));//beginIndex -- the begin index, inclusive.
+
+                                                //endIndex -- the end index, exclusive.
           }
       }
       return reversed.toString();
