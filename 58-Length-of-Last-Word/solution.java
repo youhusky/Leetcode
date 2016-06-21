c3e566d6b03c6b4305175b464f1639fec6cbb405
Bozhao Li <bozhaoli@usc.edu>
2016-06-20 17:06:51 -0700

    Leetcode

---
+++ 58-Length-of-Last-Word/solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;        
+    }
+}
