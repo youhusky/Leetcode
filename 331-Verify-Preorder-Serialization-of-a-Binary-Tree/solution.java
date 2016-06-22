ad6cccff3c626b023af3051b836b3caad02cdbea
Bozhao Li <bozhaoli@usc.edu>
2016-06-21 17:30:26 -0700

    Leetcode

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -17,3 +17,6 @@ public class Solution {
         return stack.peek()=="#"&& stack.size()==1;
     }
 }
+// //1.基本数据类型，也称原始数据类型。byte,short,char,int,long,float,double,boolean
+// 他们之间的比较，应用双等号（==）,比较的是他们的值。
+// 2.复合数据类型(类) 用euqals 比较的是值而不是内存空间
