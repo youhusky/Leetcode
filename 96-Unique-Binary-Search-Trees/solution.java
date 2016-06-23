30b22f10f65291c7c6ec742b8d5c1b19b02b6066
Bozhao Li <bozhaoli@usc.edu>
2016-06-23 15:20:01 -0700

    Leetcode

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -10,3 +10,22 @@ public class Solution {
         return count[n];
     }
 }
+// 定义Count[i] 为以[0,i]能产生的Unique Binary Tree的数目，
+
+// 如果数组为空，毫无疑问，只有一种BST，即空树，
+// Count[0] =1
+
+// 如果数组仅有一个元素{1}，只有一种BST，单个节点
+// Count[1] = 1
+
+// 如果数组有两个元素{1,2}， 那么有如下两种可能
+// 1                       2
+//   \                    /
+//     2                1
+// Count[2] = Count[0] * Count[1]   (1为根的情况)
+//                   + Count[1] * Count[0]  (2为根的情况。
+
+// 再看一遍三个元素的数组，可以发现BST的取值方式如下：
+// Count[3] = Count[0]*Count[2]  (1为根的情况)
+//               + Count[1]*Count[1]  (2为根的情况)
+//               + Count[2]*Count[0]  (3为根的情况)
