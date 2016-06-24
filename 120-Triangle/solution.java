38145352dbc2c3b0c9ee6156dc40fbef99ed96f4
Bozhao Li <bozhaoli@usc.edu>
2016-06-23 17:42:05 -0700

    Leetcode

---
+++ 120-Triangle/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.size() == 0)   return -1;
+        int[] A = new int[triangle.size()+1];
+        for(int i=triangle.size()-1;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size();j++){
+                A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
+            }
+        }
+        return A[0];
+    }
+}
