aed36c051e6b19243be285a228b0ecd066c8b0f3
Bozhao Li <bozhaoli@usc.edu>
2016-06-24 18:09:22 -0700

    Leetcode

---
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1) return 0;
+
+    int b0 = -prices[0], b1 = b0;
+    int s0 = 0, s1 = 0, s2 = 0;
+
+    for(int i = 1; i < prices.length; i++) {
+        b0 = Math.max(b1, s2 - prices[i]);
+        s0 = Math.max(s1, b1 + prices[i]);
+        b1 = b0; s2 = s1; s1 = s0; 
+    }
+    return s0;
+    }
+}
+// 2. Define Recursion
+
+// buy[i]: To make a decision whether to buy at i, we either take a rest, by just using the old decision at i - 1, or sell at/before i - 2, then buy at i, We cannot sell at i - 1, then buy at i, because of cooldown.
+// sell[i]: To make a decision whether to sell at i, we either take a rest, by just using the old decision at i - 1, or buy at/before i - 1, then sell at i.
+// So we get the following formula:
+
+// buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);   
+// sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+// 3. Optimize to O(1) Space
+
+// DP solution only depending on i - 1 and i - 2 can be optimized using O(1) space.
+
+// Let b2, b1, b0 represent buy[i - 2], buy[i - 1], buy[i]
+// Let s2, s1, s0 represent sell[i - 2], sell[i - 1], sell[i]
+// Then arrays turn into Fibonacci like recursion:
+
+// b0 = Math.max(b1, s2 - prices[i]);
+// s0 = Math.max(s1, b1 + prices[i]);
