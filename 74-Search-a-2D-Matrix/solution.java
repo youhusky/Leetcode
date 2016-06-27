2f99683c3c50a2164d5c37946e1cc2c060398ad0
Bozhao Li <bozhaoli@usc.edu>
2016-06-27 12:32:11 -0700

    Leetcode

---
+++ 74-Search-a-2D-Matrix/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int l = 0, r = m * n - 1;
+        while (l != r){
+            int mid = l + (r-l)/2;
+            if (matrix[mid / m][mid % m] < target)
+                l = mid + 1;
+            else 
+                r = mid;
+        }
+        return matrix[r / m][r % m] == target;
+    }
+}
