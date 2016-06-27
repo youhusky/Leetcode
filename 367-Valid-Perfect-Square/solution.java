a1cbe67aa7b6792b75ee2f36ae39f9577546436b
Bozhao Li <bozhaoli@usc.edu>
2016-06-27 14:35:54 -0700

    Leetcode

---
+++ 367-Valid-Perfect-Square/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long x = num;
+        while (x * x > num) {
+            x = (x + num / x) / 2;
+        }
+        return x * x == num;
+    }
+}
