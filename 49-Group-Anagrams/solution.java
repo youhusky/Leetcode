b274fcced13f46ffdf9ad9e668067b0ab26766f2
Bozhao Li <bozhaoli@usc.edu>
2016-06-30 21:54:09 -0700

    Leetcode

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+
+    if (strs == null || strs.length == 0) {
+        return res;
+    } 
+
+    Map<String, Integer> m = new HashMap<>();
+
+    for (String str : strs) {
+        String sortedStr = sortString(str);
+
+        if (m.containsKey(sortedStr)) {
+            int index = m.get(sortedStr);
+            res.get(index).add(str);
+        } else {
+            m.put(sortedStr, res.size());
+            List<String> group = new ArrayList<>();
+            group.add(str);
+            res.add(group);
+        }
+    }
+
+    return res;
+}
+
+private String sortString (String str) {
+    char[] chars = str.toCharArray();
+    Arrays.sort(chars);
+    return new String(chars);
+}
+}
