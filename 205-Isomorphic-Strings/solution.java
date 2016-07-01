822ccca5422b7842b6f5922521cf44eb6e304f96
Bozhao Li <bozhaoli@usc.edu>
2016-06-30 21:30:05 -0700

    Leetcode

---
+++ 205-Isomorphic-Strings/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null || s.length() <= 1) return true;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        for(int i = 0 ; i< s.length(); i++){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a)){
+                 if(map.get(a).equals(b))
+                continue;
+                else
+                return false;
+            }else{
+                if(!map.containsValue(b))
+                map.put(a,b);
+                else return false;
+
+            }
+        }
+        return true;
+    }
+}
