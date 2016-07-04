ad30ec00ce13c41a91d0159c67358ad3edfc46d3
Bozhao Li <bozhaoli@usc.edu>
2016-07-04 15:01:55 -0700

    Leetcode

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,46 +1,49 @@
 public class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
-        List<int[]> res = new ArrayList();
-        List<int[]> height = new ArrayList();
-        
-        for(int[] b : buildings){
-            height.add(new int[]{b[0],-b[2]});
+        List<int[]> result = new ArrayList<>();
+        List<int[]> height = new ArrayList<>();
+        // 拆解矩形，构建顶点的列表
+        for(int[] b:buildings) {
+            // 左顶点存为负数
+            height.add(new int[]{b[0], -b[2]});
+            // 右顶点存为正数
             height.add(new int[]{b[1], b[2]});
         }
-        
+        // 根据横坐标对列表排序，相同横坐标的点纵坐标小的排在前面
         Collections.sort(height, new Comparator<int[]>(){
             public int compare(int[] a, int[] b){
-                if(a[0]!=b[0]){
+                if(a[0] != b[0]){
                     return a[0] - b[0];
-                }
-                else{
+                } else {
                     return a[1] - b[1];
                 }
             }
         });
-        
+        // 构建堆，按照纵坐标来判断大小
         Queue<Integer> pq = new PriorityQueue<Integer>(11, new Comparator<Integer>(){
             public int compare(Integer i1, Integer i2){
                 return i2 - i1;
             }
         });
-        
+        // 将地平线值9先加入堆中
         pq.offer(0);
+        // prev用于记录上次keypoint的高度
         int prev = 0;
-        for(int[] h:height){
-            if(h[1] < 0){
+        for(int[] h:height) {
+            // 将左顶点加入堆中
+            if(h[1] < 0) {
                 pq.offer(-h[1]);
-            }
-            else{
+            } else {
+            // 将右顶点对应的左顶点移去
                 pq.remove(h[1]);
             }
             int cur = pq.peek();
-            if (cur != prev){
-                res.add(new int[]{h[0],cur});
+            // 如果堆的新顶部和上个keypoint高度不一样，则加入一个新的keypoint
+            if(prev != cur) {
+                result.add(new int[]{h[0], cur});
                 prev = cur;
             }
-            
         }
-        return res;
+        return result;
     }
 }
