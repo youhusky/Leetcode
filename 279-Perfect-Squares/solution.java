8d865a195704e8a4dcf1887a3f8b4db1df443180
Bozhao Li <bozhaoli@usc.edu>
2016-07-04 15:31:25 -0700

    Leetcode

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -10,7 +10,7 @@ public class Solution {
         // 从小到大找任意数a
         for(int a = 0; a <= n; a++){
             // 从小到大找平方数bｘb
-            for(int b = 0; a + b * b <= n; b++){
+            for(int b = 1; a + b * b <= n; b++){
                 // 因为a+b*b可能本身就是平方数，所以我们要取两个中较小的
                 dp[a + b * b] = Math.min(dp[a] + 1, dp[a + b * b]);
             }
