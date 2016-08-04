f4a09fd4f8c8e17a766a7e976bd17ddc7b089afa
Bozhao Li <bozhaoli@usc.edu>
2016-08-04 14:37:51 -0700

    Leetcode

---
+++ 138-Copy-List-with-Random-Pointer/solution.cpp
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        
+    }
+};
