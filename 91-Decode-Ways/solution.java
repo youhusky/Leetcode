dbf10ce1625c92e10e303cee32dd68f2a4b4a675
Bozhao Li <bozhaoli@usc.edu>
2016-08-05 18:19:18 -0700

    Leetcode

---
+++ 91-Decode-Ways/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int numDecodings(String s) {
+        if(s.length() == 0) return s.length();
+        int[] dp = new int[s.length() + 1];
+        // 初始化第一种解码方式
+        dp[0] = 1;
+        // 如果第一位是0，则无法解码
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i = 2; i <= s.length(); i++){
+            // 如果字符串的第i-1位和第i位能组成一个10到26的数字，说明我们可以在第i-2位的解码方法上继续解码
+            if(Integer.parseInt(s.substring(i-2, i)) <= 26 && Integer.parseInt(s.substring(i-2, i)) >= 10){
+                dp[i] += dp[i - 2];
+            }
+            // 如果字符串的第i-1位和第i位不能组成有效二位数字，在第i-1位的解码方法上继续解码
+            if(Integer.parseInt(s.substring(i-1, i)) != 0){
+                dp[i] += dp[i - 1];
+            }
+        }
+        return dp[s.length()];
+    }
+}
