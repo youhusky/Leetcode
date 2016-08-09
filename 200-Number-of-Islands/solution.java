756ed960bd1620a531271156717fa2895c492883
Bozhao Li <bozhaoli@usc.edu>
2016-08-08 17:48:41 -0700

    Leetcode

---
+++ 200-Number-of-Islands/solution.java
@@ -0,0 +1,36 @@
+
+    public class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        if (rows == 0) {
+            return 0;
+        }
+        int cols = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+    
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+        
+        // 访问过的做标记
+        grid[row][col] = '0';   
+        
+        // 搜邻居
+        dfs(grid, row - 1, col);
+        dfs(grid, row + 1, col);
+        dfs(grid, row, col - 1);
+        dfs(grid, row, col + 1);
+    }
+}
+
