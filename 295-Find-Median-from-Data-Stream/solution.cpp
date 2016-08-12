cf948bb81aae2a2a68772bc2bd4143f8d62c62c4
Bozhao Li <bozhaoli@usc.edu>
2016-08-12 16:56:44 -0700

    Leetcode

---
+++ 295-Find-Median-from-Data-Stream/solution.cpp
@@ -0,0 +1,18 @@
+class MedianFinder {
+public:
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        
+    }
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf;
+// mf.addNum(1);
+// mf.findMedian();
