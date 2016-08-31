6460e8c8e91c9af3e7f88e6a52d8139321f19a36
Bozhao Li <bozhaoli@usc.edu>
2016-08-31 16:13:42 -0700

    Leetcode

---
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<int[]> result = new ArrayList<int[]>();
+ 
+    k = Math.min(k, nums1.length*nums2.length);
+ 
+    if(k==0)
+        return result;
+ 
+    int[] idx = new int[nums1.length];
+ 
+    while(k>0){
+        int min = Integer.MAX_VALUE;
+        int t=0;
+        for(int i=0; i<nums1.length; i++){
+            if(idx[i]<nums2.length && nums1[i]+nums2[idx[i]]<min){
+                t=i;
+                min = nums1[i]+nums2[idx[i]];
+            }
+        }
+ 
+        int[] arr = {nums1[t], nums2[idx[t]]};    
+        result.add(arr);
+ 
+        idx[t]++;
+ 
+        k--;
+    }
+ 
+    return result;
+    }
+}
