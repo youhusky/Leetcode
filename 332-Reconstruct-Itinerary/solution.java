d657d0b2830fc87e9c312305297455ffa208ae6a
Bozhao Li <bozhaoli@usc.edu>
2016-08-31 15:47:40 -0700

    Leetcode

---
+++ 332-Reconstruct-Itinerary/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    Map<String, PriorityQueue<String>> flights;
+    LinkedList<String> path;
+
+    public List<String> findItinerary(String[][] tickets) {
+        flights = new HashMap<>();
+        path = new LinkedList<>();
+        for (String[] ticket : tickets) {
+            flights.putIfAbsent(ticket[0], new PriorityQueue<>());
+            flights.get(ticket[0]).add(ticket[1]);
+        }
+        dfs("JFK");
+        return path;
+    }
+
+    public void dfs(String departure) {
+        PriorityQueue<String> arrivals = flights.get(departure);
+        while (arrivals != null && !arrivals.isEmpty())
+            dfs(arrivals.poll());
+        path.addFirst(departure);
+    }
+}
