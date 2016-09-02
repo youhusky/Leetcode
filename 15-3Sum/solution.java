8048a36ec85e0204d4c7f017703dc66e6b4f6732
Bozhao Li <bozhaoli@usc.edu>
2016-09-02 11:30:53 -0700

    Leetcode

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,20 +1,35 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        if (nums.length < 3)
+            return res;
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i=0; i< nums.length; i++){
-            if (i != 0 && nums[i] == nums[i-1]) continue; // i< nums.length -1 -> [0,0,0] error
-            int left = i+1;
-            int right = nums.length - 1;
-            while(left < right){
+        for (int i = 0; i < nums.length-2; i++){
+            int left = i + 1, right = nums.length - 1;
+            if (i!=0 && nums[i] == nums[i-1])
+                continue;
+            while (left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0){
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-                    res.add(Arrays.asList(nums[i], nums[left++], nums[right--])); // add the result and add its value
+                    ArrayList<Integer> temp = new ArrayList();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    res.add(temp);
+                    left ++;
+                    right --;
+                    //avoid duplicate
+                    while(left < right && nums[left] == nums[left - 1])
+                        left ++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
                 }
-                else if(sum<0) left++;
-                else if(sum>0) right--;
+                else if (sum < 0){
+                    left ++;
+                }
+                else{
+                    right --;
+                }
             }
         }
         return res;
