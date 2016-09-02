f6097cec17e27ee60cfc6d9381870ba1518dd87e
Bozhao Li <bozhaoli@usc.edu>
2016-09-02 16:15:36 -0700

    Leetcode

---
+++ 18-4Sum/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        if(num.length<4)return ans;
+        Arrays.sort(num);
+        for(int i=0; i<num.length-3; i++){
+            if(i>0&&num[i]==num[i-1])continue;
+            for(int j=i+1; j<num.length-2; j++){
+                if(j>i+1&&num[j]==num[j-1])continue;
+                int low=j+1, high=num.length-1;
+                while(low<high){
+                    int sum=num[i]+num[j]+num[low]+num[high];
+                    if(sum==target){
+                        ans.add(Arrays.asList(num[i], num[j], num[low], num[high]));
+                        while(low<high&&num[low]==num[low+1])low++;
+                        while(low<high&&num[high]==num[high-1])high--;
+                        low++;
+                        high--;
+                    }
+                    else if(sum<target)low++;
+                    else high--;
+                }
+            }
+        }
+        return ans;
+    }
+}
