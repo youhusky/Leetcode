0e984c996ceca9437b9f0d13370fd75f4c9e5e7b
Bozhao Li <bozhaoli@usc.edu>
2016-09-13 12:57:24 -0700

    Leetcode

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,14 +1,18 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] res = new int[2];
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for (int i =0; i< nums.length;i++){
-            if (map.containsKey(target - nums[i])){
+        int[] res = new int[2];
+        if (nums == null || nums.length < 2){
+            return res;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();// <remain, index>
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])){
+                res[0] = map.get(nums[i]);
                 res[1] = i;
-                res[0] = map.get(target - nums[i])-1;
                 return res;
+            }else{
+                map.put(target - nums[i], i);
             }
-            map.put(nums[i], i+1);
         }
         return res;
     }
